425d36af024d803e25e47a93b3bd650ea11cbb7b
Maik Catrinque <maik@simbioseventures.com>
2019-04-13 13:41:37 -0300

    10528 - Major Scales.cpp

--- 10528 - Major Scales.cpp
+++ 10528 - Major Scales.cpp
@@ -7,10 +7,9 @@
 
 using namespace std;
 
-int main()
-{
+int main() {
     string line;
-    vector<string> scale = { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
+    vector <string> scale = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
 
     getline(cin, line);
 
@@ -18,7 +17,7 @@ int main()
         string note;
         stringstream ss;
         ss << line;
-        vector<string> notes;
+        vector <string> notes;
 
         while (ss >> note) {
             notes.push_back(note);
@@ -29,10 +28,10 @@ int main()
             int counter = 0;
             for (int j = 0; j < notes.size(); j++) {
                 if (notes[j] == scale[p % 12] || notes[j] == scale[(p + 2) % 12] || notes[j] == scale[(p + 4) % 12]
-                    || notes[j] == scale[(p + 5) % 12] || notes[j] == scale[(p + 7) % 12] || notes[j] == scale[(p + 9) % 12] || notes[j] == scale[(p + 11) % 12]) {
+                    || notes[j] == scale[(p + 5) % 12] || notes[j] == scale[(p + 7) % 12] ||
+                    notes[j] == scale[(p + 9) % 12] || notes[j] == scale[(p + 11) % 12]) {
                     counter++;
-                }
-                else
+                } else
                     break;
             }
 

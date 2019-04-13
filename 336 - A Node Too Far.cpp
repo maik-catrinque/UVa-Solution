3b57441ddb7021605c9929d5355524196c602bbc
Maik Catrinque <maik@simbioseventures.com>
2019-04-13 13:23:40 -0300

    336 - A Node Too Far.cpp

--- 336 - A Node Too Far.cpp
+++ 336 - A Node Too Far.cpp
@@ -5,53 +5,52 @@
 #include <map>
 
 using namespace std;
-map <int,int> visited;
+map<int, int> visited;
 
-void bfs(int start, map <int,vector<int> > graph) {
-    queue<int> s;
-    s.push(start);
-    visited[start]=0;
-    while (s.empty() == false) {
-        int top = s.front();
-        s.pop();
-        int size = graph[top].size();
-        for(int i=0;i<size;i++){
-            int n =graph[top][i];
-            if(!visited.count(n)){
-                visited[n]=visited[top]+1; //Number of moves until here + 1
-                s.push(n);
-            }
-        }
-     }
+void bfs(int start, map<int, vector<int>> graph) {
+  queue<int> s;
+  s.push(start);
+  visited[start] = 0;
+  while (s.empty() == false) {
+    int top = s.front();
+    s.pop();
+    int size = graph[top].size();
+    for (int i = 0; i < size; i++) {
+      int n = graph[top][i];
+      if (!visited.count(n)) {
+        visited[n] = visited[top] + 1;  // Number of moves until here + 1
+        s.push(n);
+      }
+    }
+  }
 }
 
-int main(){
-    int weight = 1;
-    int nodes,a,b,cases=1;
+int main() {
+  int weight = 1;
+  int nodes, a, b, cases = 1;
 
-    while(cin>>nodes,nodes){
-             map <int,vector<int> > graph;
-             for(int i=0;i<nodes;i++){
-                     cin>>a>>b;
-                     graph[a].push_back(b);
-                     graph[b].push_back(a);
-             }
-             int ttl,start;
-             while(cin>>start>>ttl,(start!=0 || ttl!=0)){
-                map <int,int>::iterator it;
-                visited.clear();
-                bfs(start,graph);
-                int result = 0;
-                 for(it = visited.begin(); it != visited.end(); it++){
-                    if(it->second>ttl)
-						result++;
-                 }
-				result += graph.size()-visited.size();
-				cout<<"Case "<<cases<<": "<<result<<" nodes not reachable from node "<<start<<" with TTL = "<<ttl<<"."<<endl;
-                cases++;
-             }
-
+  while (cin >> nodes, nodes) {
+    map<int, vector<int>> graph;
+    for (int i = 0; i < nodes; i++) {
+      cin >> a >> b;
+      graph[a].push_back(b);
+      graph[b].push_back(a);
     }
- return 0;
-
+    int ttl, start;
+    while (cin >> start >> ttl, (start != 0 || ttl != 0)) {
+      map<int, int>::iterator it;
+      visited.clear();
+      bfs(start, graph);
+      int result = 0;
+      for (it = visited.begin(); it != visited.end(); it++) {
+        if (it->second > ttl) result++;
+      }
+      result += graph.size() - visited.size();
+      cout << "Case " << cases << ": " << result
+           << " nodes not reachable from node " << start
+           << " with TTL = " << ttl << "." << endl;
+      cases++;
+    }
+  }
+  return 0;
 }
